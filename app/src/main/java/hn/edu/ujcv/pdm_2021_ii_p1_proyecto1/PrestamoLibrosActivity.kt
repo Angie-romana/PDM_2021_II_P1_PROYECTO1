a2411074629af347c647d5e1409aa29dd6650f24
Mitsuki <angienicoll7@gmail.com>
2021-06-08 15:06:35 -0600

    INICIO DE PROGRAMA

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p1_proyecto1/PrestamoLibrosActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2021_ii_p1_proyecto1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PrestamoLibrosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prestamo_libros)
+    }
+}
