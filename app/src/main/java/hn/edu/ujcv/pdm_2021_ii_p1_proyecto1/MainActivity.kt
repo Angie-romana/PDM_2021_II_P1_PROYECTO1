1cadbddac1a3452f5fc24e2d6f51c24b53ee459b
Mitsuki <angienicoll7@gmail.com>
2021-06-08 23:02:49 -0600

    Boton de Libro en main (programe solo que me llevara a la pantalla Libros) Mas imagenes insertadas de fondos

--- app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p1_proyecto1/MainActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_ii_p1_proyecto1/MainActivity.kt
@@ -1,11 +1,17 @@
 package hn.edu.ujcv.pdm_2021_ii_p1_proyecto1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnLibros.setOnClickListener {
+            val intent = Intent(this, LibrosActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
